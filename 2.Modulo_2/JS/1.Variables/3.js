18e07df5f25a258989bab7cb86e0588c4e1cf0f6
César <cesarlpb@gmail.com>
2023-05-09 18:20:15 +0200

    Ej 3 - Saludar con el nombre

---
+++ 2.Modulo_2/JS/1.Variables/3.js
@@ -0,0 +1,18 @@
+/**
+ * Un poco de interactividad. En este ejercicio debes escribir un programa que lea el nombre del usuario y salude con un "Hola" seguido del nombre del usuario.
+
+Para leer el nombre usa un prompt con un texto indicativo como "Escribe tu nombre"
+
+Aparecerá el prompt y el usuario escribirá su nombre:
+
+Usuario escribe Juan
+
+Hola, Juan.
+ */
+
+// alert(), prompt(), confirm()
+
+let nombre = prompt("Introduce tu nombre:")
+let mensaje = "Hola, " + nombre
+alert(mensaje)
+
