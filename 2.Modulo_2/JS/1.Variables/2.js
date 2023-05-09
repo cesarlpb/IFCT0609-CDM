9c4d811550b9039bb716321a027ea83bdc80d18e
César <cesarlpb@gmail.com>
2023-05-09 18:12:12 +0200

    Ej 2 - área y perímetro de un cuadrado

---
+++ 2.Modulo_2/JS/1.Variables/2.js
@@ -0,0 +1,16 @@
+/*
+En una variable tienes el lado de un cuadrado, debes escribir un programa que te calcule el área y el perímetro del cuadrado.
+
+el area la calculas como lado multiplicado por lado. El perímetro es la suma de los cuatro lados.
+
+Lado 40 u
+El área es 1600 u^2
+El perímetro es 160 u
+
+*/
+
+let lado = 40
+let area = lado * lado // 1600
+let perimetro = 4 * lado // 160
+alert("Área: " + area + "\nPerímetro: " + perimetro)
+
