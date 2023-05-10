d269e7dd90ef1560329a158f6156ab1348e751ea
César <cesarlpb@gmail.com>
2023-05-10 20:22:49 +0200

    Config para base de datos local

---
+++ 2.Modulo_2/PHP/db.php
@@ -0,0 +1,17 @@
+<?php
+// Conexión a la base de datos local
+// Variables de conexión:
+$db_host = "localhost";
+$db_user = "root"; // TODO: en el futuro, crearemos un usuario con permisos
+$db_password = "";
+$db_name = "pruebas";
+
+// Crear conexión
+$conn = msqli_connect($db_host, $db_user, $db_password, $db_name);
+
+// Comprobar si hay errores
+if(!$conn){
+    die("Error en la conexión: " . mysqli_connect_error());
+}
+echo "Conexión exitosa!";
+?>
