20b8f213e8b206e0d6a1d3e9a2e51872d55682bb
César <cesarlpb@gmail.com>
2023-05-10 18:54:25 +0200

    03. Hola 2

---
+++ 2.Modulo_2/PHP/03_hola2.php
@@ -0,0 +1,16 @@
+<html> 
+<head> 
+    <title>Hola Mundo</title> 
+</head> 
+<body> 
+    <h1> El famoso script Hello World</h1> 
+<p> 
+    <?php 
+    echo "<hr>";
+    echo "<h2>Hola Mundo!</h2>"; 
+    echo "<p>Esto es un programa de PHP!</p>";
+    echo "<hr>";
+    ?> 
+</p> 
+</body> 
+</html>
