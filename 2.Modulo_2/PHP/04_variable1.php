ef21801bc4e3ec7c32b632a52cede6527998d8e1
César <cesarlpb@gmail.com>
2023-05-10 19:02:02 +0200

    04. Variable 1

---
+++ 2.Modulo_2/PHP/04_variable1.php
@@ -0,0 +1,14 @@
+<html> 
+<head> 
+ <title></title> 
+</head> 
+<body> 
+    <h1>Primer ejemplo de Variables </h1> 
+    <p> 
+    <?php 
+        $name = "tu_nombre";
+        echo "Hola <b>$name</b>, encantado de conocerte :)"; 
+    ?> 
+    </p> 
+</body> 
+</html>
