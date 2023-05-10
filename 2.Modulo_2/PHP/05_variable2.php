340543737dd4d4c1cfa902d298e6202a3b309bc0
César <cesarlpb@gmail.com>
2023-05-10 19:46:38 +0200

    05-06 Variable 2 y break <br>

---
+++ 2.Modulo_2/PHP/05_variable2.php
@@ -0,0 +1,14 @@
+<html> 
+<head> 
+ <title>Variable 2</title> 
+</head> 
+<body> 
+    <h1>Segundo ejemplo de Variables usando el operador concatenacion .</h1> 
+    <p> 
+        <?php 
+        $name = "tu_nombre"; 
+        echo "Hola <b>" . $name . "</b>, encantado de conocerte"; 
+        ?> 
+    </p> 
+</body> 
+</html>
---
+++ 2.Modulo_2/PHP/06_break.php
@@ -0,0 +1,18 @@
+<html> 
+<head> 
+ <title>Break</title> 
+</head> 
+<body> 
+<h1>Break en la ventana del navegador </h1> 
+<p> 
+    <?php 
+    $name = "tu_nombre"; 
+    echo "Hola <b>$name</b>, encantado de conocerte"; 
+    echo "<br>";
+    echo "Salto simple";
+    echo "<br><br>";
+    echo "Salto doble"; 
+    ?> 
+</p> 
+</body> 
+</html>
