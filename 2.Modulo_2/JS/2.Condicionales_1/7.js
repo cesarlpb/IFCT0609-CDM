b968f73ebc6cf4c57fbea533261c13988dfd5785
César <cesarlpb@gmail.com>
2023-05-11 15:51:58 +0200

    Ej 7 - Elección de medio de transporte

---
+++ 2.Modulo_2/JS/2.Condicionales_1/7.js
@@ -0,0 +1,20 @@
+/*
+Este programa te ayuda a viajar. El programa pide al usuario si va a viajar con coche, tren, bicicleta o autobús. Si va en tren o autobus le recordará que lleve dinero para el billete.
+
+tren o autobús  -> llevar dinero billete
+coche           -> coge las llaves
+bicicleta       -> el casco
+
+*/
+
+let medioTransporte = prompt("Elige el medio de transporte:\ncoche\nbicicleta\ntren\nautobús")
+let opciones = ["tren", "autobús", "autobus"]
+if(opciones.includes(medioTransporte)){
+    alert("Recuerda llevar dinero para el billete")
+}else if(medioTransporte == "coche"){
+    alert("Coge las llaves!")
+}else if(medioTransporte == "bicicleta"){
+    alert("Coge el casco!")
+}else{
+    alert("Buen viaje.")
+}
