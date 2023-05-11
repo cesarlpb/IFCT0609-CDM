2903e71e3b6a792c6036aed00f8bfa7102f436f6
César <cesarlpb@gmail.com>
2023-05-11 18:46:38 +0200

    Ej 5 - validando saldo y salas del usuario

---
+++ 2.Modulo_2/JS/3.Condicionales_2/5.js
@@ -0,0 +1,28 @@
+/*
+En una sala de juegos existen tre salas: Consolas, Juegos 2D, Juegos 3D, Realidad Virtual. Si un usuario paga  4 créditos puede acceder a todas, si apga 3 solo podrá acceder a las tres primeras, si paga 2 a las dos primeras y si paga 1 solo a la primera sala.
+*/
+
+// TODO: resolver este ejercicio con los otros dos métodos
+// TODO: rehacer con arrays - yay!
+// TODO: corregir eso del 0 y del 5
+// Nota: no uses switch. Acaba mal.
+
+let saldo = parseInt(prompt("Introduce tu saldo:"))
+let salas = "" // string
+switch(saldo){
+    case 1:
+        salas = "Consolas"
+        break;
+    case 2:
+        salas = "Consolas, Juegos 2D"
+        break;
+    case 3:
+        salas = "Consolas, Juegos 2D, Juegos 3D"
+        break;
+    case 4:
+        salas = "Consolas, Juegos 2D, Juegos 3D, Realidad Virtual"
+        break;
+    default:
+        salas = "ninguna sala :("
+}
+alert("Puedes ingresar a " + salas)
