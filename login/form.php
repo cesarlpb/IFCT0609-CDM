65071e66a0e71526437bda9cc928398b3041c1a0
César <cesarlpb@gmail.com>
2023-05-17 20:29:18 +0200

    Ejemplo <<correcto>> de login con PHP
    It works!

---
+++ login/form.php
@@ -0,0 +1,11 @@
+<?php
+  $usuario = $_POST["usuario"];
+  $contraseña = $_POST["password"];
+
+  // Normalmente, se leería en la base de datos la tablar users para comprobar las credenciales
+  if($usuario == "pepito" && $contraseña == "Supersafe"){
+    echo "Has iniciado sesión";
+  }else{
+    echo "Esos datos no son correctos";
+  }
+?>
