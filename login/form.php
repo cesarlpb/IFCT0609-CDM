fd912aa8bd5c4a0d0203cd378beb82e5f13095bf
César <cesarlpb@gmail.com>
2023-05-18 15:55:28 +0200

    comentario

--- login/form.php
+++ login/form.php
@@ -5,7 +5,9 @@
   // Normalmente, se leería en la base de datos la tablar users para comprobar las credenciales
   if($usuario == "pepito" && $contraseña == "Supersafe"){
     echo "Has iniciado sesión";
+    // Seguir programando
   }else{
     echo "Esos datos no son correctos";
+    // Se acabó
   }
 ?>
