f7c64688f4b15d77966ad2bcf8037d0c34dd4927
César <cesarlpb@gmail.com>
2023-05-25 16:51:25 +0200

    Ej 1. Contar hasta n con while

--- 2.Modulo_2/JS/5.While/1.js
+++ 2.Modulo_2/JS/5.While/1.js
@@ -1,9 +1,14 @@
-let num = 0
-console.log("Antes del while")
-while(num <= 5){
-    console.log(num)
-    num++
+/*
+Usando un bucle while escribir un script que pida un valor entero y cree una lista con los números desde el 0 al valor tecleado. Luego deberá sacar esa lista con un alert. Los números se separarán por comas.  
+
+Si le doy el número 5 pues deberá contar 0,1,2,3,4,5
+*/
+
+let num = parseInt(prompt("Introduce un número entero:"))
+let i = 0, mensaje = ""
+while(i <= num){
+    mensaje += i + ", "
+    i++
 }
-console.log("Después del while")
-console.log(num) // ojo 
+alert(mensaje.slice(0,-2))
 
