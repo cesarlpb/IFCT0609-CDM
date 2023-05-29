7de024509caf9dd8ba4fa14956fe8281738037ff
César <cesarlpb@gmail.com>
2023-05-29 16:10:44 +0200

    Ej 1 - Pedir letra al usuario

---
+++ 2.Modulo_2/JS/6.Do_while/1.js
@@ -0,0 +1,8 @@
+/* 
+Realiza un script que le pida al usuario que telee la letra A (en mayúscula) y mientras no lo haga se lo vuelva a pedir. Usa un bulce do..while y la ventana alert para los mensajes
+*/
+let char = "";
+do{
+    char = prompt("Introduce la letra A:")
+}while(char != "A")
+alert("AAAAAA!")
