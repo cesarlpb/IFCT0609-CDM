b1d9529dd016f0757975f523c25b34238f1422bf
César <cesarlpb@gmail.com>
2023-05-30 16:13:29 +0200

    Ej 1 - Suma de dos números

---
+++ 2.Modulo_2/JS/7.Funciones/1.js
@@ -0,0 +1,14 @@
+/*
+Crea una función que tenga dos argumentos: num1, num2, y que deuelva como resultado la suma de ambos números. Se supone que se usarán solo números válidos. 
+Llamo a la función como sumar(3,5) deberá devolver 8.
+*/
+
+/**
+ * Función que suma dos números
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @returns {Number} suma de números
+ */
+function sumar(num1, num2){
+  return num1 + num2
+}
