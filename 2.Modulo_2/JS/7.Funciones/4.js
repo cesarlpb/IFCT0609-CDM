9cb60a069b93183283102ac230a3a9de5ffc0504
César <cesarlpb@gmail.com>
2023-05-30 16:31:35 +0200

    Ej 4 - Repetir string varias veces

---
+++ 2.Modulo_2/JS/7.Funciones/4.js
@@ -0,0 +1,14 @@
+/*
+Definir una función que cree una cadena de letras repetidas tantas veces como le digamos. La función recibe dos argumentos: la letra y el número de repeticiones.
+Al ejecutar repetir('a', 5) deberá devover la caden  aaaaa.
+*/
+
+/**
+ * Fn que repite un string varias veces
+ * @param {String} char string a repetir
+ * @param {Number} repeticiones veces a repetir
+ * @returns {String} resultado
+ */
+function repetir(char, repeticiones){
+  return char.repeat(repeticiones)
+}
