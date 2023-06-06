fa15a68967393fbed5728b90b56b047959c1753b
César <cesarlpb@gmail.com>
2023-06-06 20:46:13 +0200

    Ejercicio 1757. Recyclable and Low Fat Products

--- 2.Modulo_2/SQL50/1757.sql
+++ 2.Modulo_2/SQL50/1757.sql
@@ -6,3 +6,10 @@ INSERT INTO Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y');
 INSERT INTO Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y');
 INSERT INTO Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y');
 INSERT INTO Products (product_id, low_fats, recyclable) values ('4', 'N', 'N');
+
+-- Ejercicio: Hacer un query que devuelve los ids de los productos que son low_fats ("Y") y son recyclable ("Y")
+-- Opción 1 usando AND:
+SELECT product_id FROM Products WHERE low_fats = "Y" AND recyclable = "Y";
+-- Opción 2 igualando listas: 
+SELECT product_id FROM Products WHERE (low_fats, recyclable) = ("Y", "Y");
+
