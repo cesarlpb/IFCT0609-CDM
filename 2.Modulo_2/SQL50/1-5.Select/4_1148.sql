8405612aa7433d9f7ab9ace792513140ef4d631e
César <cesarlpb@gmail.com>
2023-06-07 15:51:24 +0200

    Ejercicios 4 y 5 de SELECT - enunciados

---
+++ 2.Modulo_2/SQL50/1-5.Select/4_1148.sql
@@ -0,0 +1,13 @@
+-- 1148 https://leetcode.com/problems/article-views-i/
+-- Enunciado
+/*
+Escriba una consulta SQL para encontrar todos los autores que vieron al menos uno de sus propios artículos.
+Devuelve la tabla de resultados ordenada por id en orden ascendente.
+El formato del resultado de la consulta está en el siguiente ejemplo.
+*/
+
+--------------
+-- SOLUCIÓN --
+--------------
+
+-- coming soon
---
+++ 2.Modulo_2/SQL50/1-5.Select/5_1683.sql
@@ -0,0 +1,13 @@
+-- 1683 https://leetcode.com/problems/invalid-tweets/
+-- Enunciado:
+/*
+Escribe una consulta SQL para encontrar los ID de los tweets no válidos. El tweet es inválido si el número de caracteres utilizados en el contenido del tweet es estrictamente superior a 15.
+Devuelva la tabla de resultados en cualquier orden.
+El formato del resultado de la consulta está en el siguiente ejemplo.
+*/
+
+--------------
+-- SOLUCIÓN --
+--------------
+
+-- coming soon
