d60bb28da86b5e20687a92ccfca7fcc6c2c943bd
César <cesarlpb@gmail.com>
2023-06-07 15:46:15 +0200

    Ej 3 - enunciado

---
+++ 2.Modulo_2/SQL50/1-5.Select/3_595.sql
@@ -0,0 +1,17 @@
+-- 595 https://leetcode.com/problems/big-countries/
+-- Enunciado:
+/*
+Un país es grande si tiene una superficie de al menos tres millones (es decir, 3000000 km2), o
+tiene una población de al menos veinticinco millones (es decir, 25000000).
+Escribe una consulta SQL para obtener el nombre, la población y la superficie de los países grandes.
+
+Devuelva la tabla de resultados en cualquier orden.
+
+El formato del resultado de la consulta está en el siguiente ejemplo.
+*/
+
+--------------
+-- SOLUCIÓN --
+--------------
+
+-- coming soon
