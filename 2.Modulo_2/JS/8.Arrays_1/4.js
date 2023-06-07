eb3cd4730c3c2cdc9846780a31bba2aaed86bc0c
César <cesarlpb@gmail.com>
2023-06-07 20:01:04 +0200

    Ej 4 - arr de números pares e impares

--- 2.Modulo_2/JS/8.Arrays_1/4.js
+++ 2.Modulo_2/JS/8.Arrays_1/4.js
@@ -1,9 +1,28 @@
 /*
-Diseña un script que vaya leyendo números y guardándolos en un array. Una vez lleno mostrará el array y deberá decir cuantos números son pares y cuantos son impares. La entrada de datos termina cuando el usuario teclea 0 o un valor no numérico.
+Diseña un script que vaya leyendo números y guardándolos en un array. 
+
+Una vez lleno mostrará el array y deberá decir cuantos números son pares y cuantos son impares. 
+
+La entrada de datos termina cuando el usuario teclea 0 o un valor no numérico.
 
 Intenta resolverlo sin usar métodos del objeto array, solo indices y longitudes de arrays.
 
 No debe aceptar valores no numéricos (isNaN(valor) permite detectar valores no numéricos).
 
-Por ejemplo si tecleo: 3,5,4,6,7,8, 11el script me dirá que el array de entrada es [3,5,4,6,7,8, 11] y que hay 3 pares y 4 impares.
+Por ejemplo si tecleo: 3,5,4,6,7,8, 11 el script me dirá que el array de entrada es [3,5,4,6,7,8, 11] y que hay 3 pares y 4 impares.
 */
+
+let num, arr = [], pares = 0, impares = 0;
+do{
+  num = parseInt(prompt("Introduce un número entero:"))
+  if(!isNaN(num) && num != 0){
+    if(num % 2 == 0){ pares++ }
+    else { impares++ }
+    arr.push(num)
+  }
+}while(num != 0)
+alert(`
+  ${arr}
+  pares: ${pares}
+  impares: ${impares}
+`)
