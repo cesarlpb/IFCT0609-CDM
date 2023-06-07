9af1cd8f7282b872fdf3164b3507371da1ac60d8
César <cesarlpb@gmail.com>
2023-06-07 18:20:20 +0200

    Enunciados del 1 al 5 - tema 8 Arrays básico

---
+++ 2.Modulo_2/JS/8.Arrays_1/3.js
@@ -0,0 +1,4 @@
+/*
+Sin usar métodos del objeto array, diseña una función llamada sumaLista() capaz de sumar todos los números que forman el array que se le pase como argumento.
+Si ejecuto sumaLista([2,4,5,1,2]) deberá devolver como resultado 14
+*/
---
+++ 2.Modulo_2/JS/8.Arrays_1/4.js
@@ -0,0 +1,9 @@
+/*
+Diseña un script que vaya leyendo números y guardándolos en un array. Una vez lleno mostrará el array y deberá decir cuantos números son pares y cuantos son impares. La entrada de datos termina cuando el usuario teclea 0 o un valor no numérico.
+
+Intenta resolverlo sin usar métodos del objeto array, solo indices y longitudes de arrays.
+
+No debe aceptar valores no numéricos (isNaN(valor) permite detectar valores no numéricos).
+
+Por ejemplo si tecleo: 3,5,4,6,7,8, 11el script me dirá que el array de entrada es [3,5,4,6,7,8, 11] y que hay 3 pares y 4 impares.
+*/
