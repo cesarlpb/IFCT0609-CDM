7e8615ffafb0274c31e72098ee03461f37cd6bcb
César <cesarlpb@gmail.com>
2023-06-07 16:36:22 +0200

    edit

--- 2.Modulo_2/SQL50/1-5.Select/2_584.sql
+++ 2.Modulo_2/SQL50/1-5.Select/2_584.sql
@@ -24,4 +24,4 @@ SELECT `name` FROM Customer WHERE referee_id != 2;
 -- Usando array:
 SELECT `name` FROM Customer WHERE referee_id NOT IN (2);
 -- LeetCode:
-SELECT `name` FROM Customer WHERE referee_id != 2 OR referee_id IS null;
+SELECT `name` FROM Customer WHERE referee_id != 2 OR referee_id IS NULL;
