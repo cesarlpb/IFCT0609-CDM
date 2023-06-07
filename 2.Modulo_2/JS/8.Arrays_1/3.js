77d02eb4d09c4e0f74720c66b782a038cca07744
César <cesarlpb@gmail.com>
2023-06-07 19:54:24 +0200

    Ej 3 - suma de números en array

--- 2.Modulo_2/JS/8.Arrays_1/3.js
+++ 2.Modulo_2/JS/8.Arrays_1/3.js
@@ -1,4 +1,22 @@
 /*
 Sin usar métodos del objeto array, diseña una función llamada sumaLista() capaz de sumar todos los números que forman el array que se le pase como argumento.
+
 Si ejecuto sumaLista([2,4,5,1,2]) deberá devolver como resultado 14
 */
+
+/**
+ * Función para sumar los valores de un arr si son números
+ * @param {Array} arr 
+ * @returns {Number} valor de la suma
+ */
+function sumaLista(arr){
+  let suma = 0
+  for(let i = 0; i<arr.length; i++){
+    // En caso de que el elemento del arr no sea número, lo ignora en la suma
+    if(!isNaN(arr[i]))
+    {
+      suma += arr[i]
+    }
+  }
+  return suma
+}
