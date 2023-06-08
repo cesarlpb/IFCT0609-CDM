36a77bc5d637aad6781950b3391126b5ed4ff33a
César <cesarlpb@gmail.com>
2023-06-08 20:20:37 +0200

    Ej 10 - Tiempo medio de procesos con SELF JOIN

--- 2.Modulo_2/SQL50/6-14.Joins/10_1661.sql
+++ 2.Modulo_2/SQL50/6-14.Joins/10_1661.sql
@@ -11,4 +11,31 @@ La tabla resultante debe tener el id_máquina junto con el tiempo medio como tie
 Devuelve la tabla de resultados en cualquier orden.
 
 El formato del resultado de la consulta está en el siguiente ejemplo.
-*/
+*/  
+
+CREATE TABLE IF NOT EXISTS Activity (machine_id INT, process_id INT, activity_type ENUM('start', 'end'), timestamp FLOAT);
+TRUNCATE TABLE Activity;
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('0', '0', 'start', '0.712');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('0', '0', 'end', '1.52');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('0', '1', 'start', '3.14');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('0', '1', 'end', '4.12');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('1', '0', 'start', '0.55');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('1', '0', 'end', '1.55');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('1', '1', 'start', '0.43');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('1', '1', 'end', '1.42');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('2', '0', 'start', '4.1');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('2', '0', 'end', '4.512');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('2', '1', 'start', '2.5');
+INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES ('2', '1', 'end', '5');
+
+--------------
+-- SOLUCIÓN --
+--------------
+
+SELECT A.machine_id, TRUNCATE(SUM(B.timestamp - A.timestamp)/COUNT(*), 3) AS diff
+FROM Activity A, Activity B
+WHERE 
+  A.process_id = B.process_id 
+  AND B.timestamp - A.timestamp > 0
+  AND A.machine_id = B.machine_id
+GROUP BY machine_id;
