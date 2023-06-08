bf5663bf8ce69df9f97892d737cafb8b6ef63da4
César <cesarlpb@gmail.com>
2023-06-08 20:40:56 +0200

    Ejercicios 19 y 20 de Aggregate Functions

---
+++ 2.Modulo_2/SQL50/19-27.Aggregate/19_620.sql
@@ -0,0 +1,10 @@
+-- 620. Not Boring Movies
+-- https://leetcode.com/problems/not-boring-movies/
+
+/*
+Escriba una consulta SQL para informar de las películas con un ID impar y una descripción que no sea "aburrida".
+
+Devuelva la tabla de resultados ordenada por calificación en orden descendente.
+
+El formato del resultado de la consulta está en el siguiente ejemplo.
+*/
---
+++ 2.Modulo_2/SQL50/19-27.Aggregate/20_1251.sql
@@ -0,0 +1,10 @@
+-- 1251. Average Selling Price
+-- https://leetcode.com/problems/average-selling-price/
+
+/*
+Escriba una consulta SQL para encontrar el precio medio de venta de cada producto. average_price debe redondearse a 2 decimales.
+
+Devuelva la tabla de resultados en cualquier orden.
+
+El formato del resultado de la consulta está en el siguiente ejemplo.
+*/
