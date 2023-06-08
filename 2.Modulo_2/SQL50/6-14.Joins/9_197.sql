9ccdd54ecd4689660d5c6e2cf049e6a9b0e8ce21
César <cesarlpb@gmail.com>
2023-06-08 19:07:54 +0200

    Ej 9 - Solución con SELF JOIN

--- 2.Modulo_2/SQL50/6-14.Joins/9_197.sql
+++ 2.Modulo_2/SQL50/6-14.Joins/9_197.sql
@@ -7,4 +7,20 @@ Escriba una consulta SQL para encontrar todos los Id de fechas con temperaturas 
 Devuelve la tabla de resultados en cualquier orden.
 
 El formato del resultado de la consulta está en el siguiente ejemplo.
-*/
+*/  
+
+CREATE TABLE IF NOT EXISTS Weather (id INT, recordDate DATE, temperature INT);
+TRUNCATE TABLE Weather;
+INSERT INTO Weather (id, recordDate, temperature) VALUES ('1', '2015-01-01', '10');
+INSERT INTO Weather (id, recordDate, temperature) VALUES ('2', '2015-01-02', '25');
+INSERT INTO Weather (id, recordDate, temperature) VALUES ('3', '2015-01-03', '20');
+INSERT INTO Weather (id, recordDate, temperature) VALUES ('4', '2015-01-04', '30');
+
+--------------
+-- SOLUCIÓN --
+--------------
+
+SELECT A.id 
+FROM Weather A, Weather B
+WHERE A.recordDate=DATE_ADD(B.recordDate, INTERVAL 1 DAY)
+AND A.temperature > B.temperature; 
