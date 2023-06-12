c873d776708f0067427c99fca58ca9a5067348e7
César <cesarlpb@gmail.com>
2023-06-12 16:03:36 +0200

    console.log

--- 2.Modulo_2/Node/0.miPrimerNode.js
+++ 2.Modulo_2/Node/0.miPrimerNode.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   // res.writeHead(200, {'Content-Type': 'text/html'});
+  console.log("Hola desde consola.")
   res.end(`
   Hola 
   desde 
