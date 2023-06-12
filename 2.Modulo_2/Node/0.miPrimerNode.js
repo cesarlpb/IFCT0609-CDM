658e9158cd646b925e2e2f498688bf89e2d18209
César <cesarlpb@gmail.com>
2023-06-12 15:58:13 +0200

    cambio

--- 2.Modulo_2/Node/0.miPrimerNode.js
+++ 2.Modulo_2/Node/0.miPrimerNode.js
@@ -2,5 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   // res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hola desde Node :)');
+  res.end(`
+  Hola 
+  desde 
+  Node :)`);
 }).listen(8080);
