447572ff641a08c9ef445fad82152d1d0eac4f46
César <cesarlpb@gmail.com>
2023-06-12 20:27:44 +0200

    Enunciados

--- 2.Modulo_2/Node/4.mysql.js
+++ 2.Modulo_2/Node/4.mysql.js
@@ -6,26 +6,34 @@ const con = mysql.createConnection({
   password: ""
 });
 
-const sql = "SELECT * FROM pruebas.tblUsuarios;";
-const sql2 = "SELECT * FROM pruebas.tblUsuarios WHERE idx = 2;";
+const readAll = "SELECT * FROM pruebas.tblUsuarios;";
+const readOne = function (id){
+  return `SELECT * FROM pruebas.tblUsuarios WHERE idx = ${id};`;
+}
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Conexión exitosa a la base de datos");
 });
 
-con.query(sql, function (err, result) {
+con.query(readAll, function (err, result) {
   if (err) throw err;
   const json = JSON.parse(JSON.stringify(result, null, 2));
   console.log("Query 1:")
   console.log(json[0]);
 });
 
-con.query(sql2, function (err, result) {
+con.query(readOne(2), function (err, result) {
   if (err) throw err;
   const json = JSON.parse(JSON.stringify(result, null, 2));
   console.log("Query 2:")
   console.log(json);
 });
  
+// Query para escribir los usuarios con ids del 3 al 7
+
+// Query para escribir usuarios con móvil marca LG
+
+// Query para actualizar un campo de un usuario
+
 con.end();
