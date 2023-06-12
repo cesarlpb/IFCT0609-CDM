74b7b590e301c06ce7d440cfc5f2cee32191f774
César <cesarlpb@gmail.com>
2023-06-12 16:40:27 +0200

    Response text/plain

--- 2.Modulo_2/Node/1.text.js
+++ 2.Modulo_2/Node/1.text.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   console.log("Hola desde consola.")
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
   res.end(`
-  Hola 
-  desde 
-  Node :)`);
+  <h1>Título 1</h1>
+  `);
 }).listen(8080, () => { console.log("Servidor en ejecución en http://localhost:8080/")});
