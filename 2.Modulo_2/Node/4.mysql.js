1454c94ec05960b4d07047130d37abe09e4823b3
César <cesarlpb@gmail.com>
2023-06-12 20:23:48 +0200

    Query SELECT de ejemplo

--- 2.Modulo_2/Node/4.mysql.js
+++ 2.Modulo_2/Node/4.mysql.js
@@ -7,6 +7,7 @@ const con = mysql.createConnection({
 });
 
 const sql = "SELECT * FROM pruebas.tblUsuarios;";
+const sql2 = "SELECT * FROM pruebas.tblUsuarios WHERE idx = 2;";
 
 con.connect(function(err) {
   if (err) throw err;
@@ -16,7 +17,15 @@ con.connect(function(err) {
 con.query(sql, function (err, result) {
   if (err) throw err;
   const json = JSON.parse(JSON.stringify(result, null, 2));
+  console.log("Query 1:")
   console.log(json[0]);
+});
+
+con.query(sql2, function (err, result) {
+  if (err) throw err;
+  const json = JSON.parse(JSON.stringify(result, null, 2));
+  console.log("Query 2:")
+  console.log(json);
 });
  
 con.end();
