c8ed6267f9ef91559acdd39f41a70a78c7f97a04
César <cesarlpb@gmail.com>
2023-06-12 20:59:19 +0200

    Más queries de ejemplo

--- 2.Modulo_2/Node/4.mysql.js
+++ 2.Modulo_2/Node/4.mysql.js
@@ -10,6 +10,15 @@ const readAll = "SELECT * FROM pruebas.tblUsuarios;";
 const readOne = function (id){
   return `SELECT * FROM pruebas.tblUsuarios WHERE idx = ${id};`;
 }
+const readBetween = function(from, to){
+  return `SELECT * FROM pruebas.tblUsuarios WHERE idx >= ${from} AND idx <= ${to};`;
+}
+const queryMarca = function(marca){
+  return `SELECT * FROM pruebas.tblUsuarios WHERE marca = '${marca}'`;
+}
+const queryUpdate = function(idx, campo, valor){
+  return `UPDATE pruebas.tblusuarios SET ${campo} = '${valor}' WHERE tblusuarios.idx = '${idx}';`;
+}
 
 con.connect(function(err) {
   if (err) throw err;
@@ -31,9 +40,24 @@ con.query(readOne(2), function (err, result) {
 });
  
 // Query para escribir los usuarios con ids del 3 al 7
-
+con.query(readBetween(3, 7), function (err, result) {
+  if (err) throw err;
+  const json = JSON.parse(JSON.stringify(result, null, 2));
+  console.log("Query 3:")
+  console.log(json);
+});
 // Query para escribir usuarios con móvil marca LG
-
+con.query(queryMarca("LG"), function (err, result) {
+  if (err) throw err;
+  const json = JSON.parse(JSON.stringify(result, null, 2));
+  console.log("Query 4:")
+  console.log(json);
+});
 // Query para actualizar un campo de un usuario
-
+con.query(queryUpdate(1, "nombre", "pepe"), function (err, result) {
+  if (err) throw err;
+  const json = JSON.parse(JSON.stringify(result, null, 2));
+  console.log("Query 5:")
+  console.log(json);
+});
 con.end();
