c004c96eea7826b21a0f0cb746a09be61df17f6a
César <cesarlpb@gmail.com>
2023-06-13 16:37:19 +0200

    Solución ej 11

--- 2.Modulo_2/SQL50/11-14.Joins/11_577.sql
+++ 2.Modulo_2/SQL50/11-14.Joins/11_577.sql
@@ -9,6 +9,7 @@ Devuelva la tabla de resultados en cualquier orden.
 El formato del resultado de la consulta está en el siguiente ejemplo.
 */
 
+-- Creamos las tablas:
 CREATE TABLE IF NOT EXISTS Employee (empId INT, name VARCHAR(255), supervisor INT, salary INT);
 CREATE TABLE IF NOT EXISTS Bonus (empId INT, bonus INT);
 TRUNCATE TABLE Employee;
@@ -19,3 +20,12 @@ INSERT INTO Employee (empId, name, supervisor, salary) VALUES ('4', 'Thomas', '3
 TRUNCATE TABLE Bonus;
 INSERT INTO Bonus (empId, bonus) VALUES ('2', '500');
 INSERT INTO Bonus (empId, bonus) VALUES ('4', '2000');
+
+--------------
+-- SOLUCIÓN --
+--------------
+
+SELECT e.name, b.bonus
+FROM Employee e LEFT JOIN Bonus b ON 
+e.`empId`=b.`empId`
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
