1ee1b6114b1b653e7a3d3eb61b48ddf8c938f6f0
César <cesarlpb@gmail.com>
2023-06-13 18:08:46 +0200

    Explicación ej 12

--- 2.Modulo_2/SQL50/11-14.Joins/12_1280.sql
+++ 2.Modulo_2/SQL50/11-14.Joins/12_1280.sql
@@ -44,3 +44,57 @@ LEFT JOIN Examinations e
 ON s.student_id = e.student_id AND Subjects.subject_name = e.subject_name
 GROUP BY s.student_id, s.student_name, Subjects.subject_name
 ORDER BY s.student_id, Subjects.subject_name;
+
+-- Explicación --
+
+-- Hacemos el JOIN que equivale al CROSS JOIN, es decir, me devuelve todas las combinaciones de datos entre las tablas:
+SELECT *
+FROM Students
+JOIN Subjects;
+-- Hacemos el LEFT JOIN:
+SELECT *
+FROM Students
+JOIN Subjects
+LEFT JOIN Examinations
+ON Students.student_id = Examinations.student_id
+  AND Subjects.subject_name = Examinations.subject_name;
+-- Seleccionamos las columnas que vamos a mostrar:
+SELECT
+    Students.student_id,
+    Students.student_name,
+    Subjects.subject_name
+FROM Students
+JOIN Subjects
+LEFT JOIN Examinations
+ON 
+  Students.student_id = Examinations.student_id
+  AND 
+  Subjects.subject_name = Examinations.subject_name;
+-- Añadimos el GROUP BY:
+SELECT
+  Students.student_id,
+  Students.student_name,
+  Subjects.subject_name,
+  COUNT(Examinations.subject_name) AS attended_exams
+FROM Students
+JOIN Subjects
+LEFT JOIN Examinations
+ON Students.student_id = Examinations.student_id
+AND Subjects.subject_name = Examinations.subject_name
+GROUP BY Students.student_id, Subjects.subject_name;
+
+-- Añadimos ORDER BY:
+SELECT
+    Students.student_id,
+    Students.student_name,
+    Subjects.subject_name,
+    COUNT(Examinations.subject_name) AS attended_exams
+FROM Students
+JOIN Subjects
+LEFT JOIN Examinations
+ON Students.student_id = Examinations.student_id
+AND Subjects.subject_name = Examinations.subject_name
+GROUP BY Students.student_id, Subjects.subject_name
+ORDER BY student_id ASC, subject_name ASC;
+
+-- Referencia: https://leetcode.com/problems/students-and-examinations/solutions/2564378/simple-solution-with-explanations/?envType=study-plan-v2&envId=top-sql-50
