824027455b963cf7a72588770198e1255acd62df
César <cesarlpb@gmail.com>
2023-06-13 17:29:13 +0200

    Solución ej 12 - CROSS JOIN y LEFT JOIN

--- 2.Modulo_2/SQL50/11-14.Joins/12_1280.sql
+++ 2.Modulo_2/SQL50/11-14.Joins/12_1280.sql
@@ -8,6 +8,7 @@ Devuelve la tabla de resultados ordenada por student_id y subject_name.
 
 El formato del resultado de la consulta es el del siguiente ejemplo
 */
+-- Creamos las tablas del ejercicio:
 CREATE TABLE IF NOT EXISTS Students (student_id INT, student_name VARCHAR(20));
 CREATE TABLE IF NOT EXISTS Subjects (subject_name VARCHAR(20));
 CREATE TABLE IF NOT EXISTS Examinations (student_id INT, subject_name VARCHAR(20));
@@ -32,3 +33,14 @@ INSERT INTO Examinations (student_id, subject_name) VALUES ('13', 'Programming')
 INSERT INTO Examinations (student_id, subject_name) VALUES ('13', 'Physics');
 INSERT INTO Examinations (student_id, subject_name) VALUES ('2', 'Math');
 INSERT INTO Examinations (student_id, subject_name) VALUES ('1', 'Math');
+
+--------------
+-- SOLUCIÓN --
+--------------
+
+SELECT s.student_id, s.student_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s CROSS JOIN Subjects 
+LEFT JOIN Examinations e
+ON s.student_id = e.student_id AND Subjects.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, Subjects.subject_name
+ORDER BY s.student_id, Subjects.subject_name;
