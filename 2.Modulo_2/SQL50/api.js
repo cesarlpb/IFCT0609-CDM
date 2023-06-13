d84087284e36a32495b103221a9249e3da3d8b32
César <cesarlpb@gmail.com>
2023-06-13 18:57:09 +0200

    comentarios de uso

--- 2.Modulo_2/SQL50/api.js
+++ 2.Modulo_2/SQL50/api.js
@@ -1,7 +1,7 @@
 // Script de node para mostrar las salidas de los queries de SQL50
 // Cómo usar:
 // npm install en esta carpeta
-// Pegar el query en la variable query
+// Pegar el query en la variable query en la línea 20 y colocad la base de datos en la línea 16
 // node api.js
 // Abrir el navegador en localhost:8080/api
 
@@ -12,11 +12,17 @@ const url = require('url');
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: ""
+  password: "",
+  database: "db" // <--- colocad la base de datos que usáis
 });
 
 // Query a ejecutar
-const query = "SELECT * FROM leetcode.Employees;";
+const query = `SELECT s.student_id, s.student_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s CROSS JOIN Subjects 
+LEFT JOIN Examinations e
+ON s.student_id = e.student_id AND Subjects.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, Subjects.subject_name
+ORDER BY s.student_id, Subjects.subject_name;`;
 
 http.createServer(function (req, res) {
   // Analiza la URL en busca de parámetros
