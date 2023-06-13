3f9801b515baab2841caa65fef1bf7c7ed96976f
César <cesarlpb@gmail.com>
2023-06-13 20:54:17 +0200

    Ej 14 - Solución con JOIN y explicación

--- 2.Modulo_2/SQL50/11-14.Joins/14_1934.sql
+++ 2.Modulo_2/SQL50/11-14.Joins/14_1934.sql
@@ -25,3 +25,98 @@ INSERT INTO Confirmations (user_id, time_stamp, action) VALUES ('7', '2021-06-13
 INSERT INTO Confirmations (user_id, time_stamp, action) VALUES ('7', '2021-06-14 13:59:27', 'confirmed');
 INSERT INTO Confirmations (user_id, time_stamp, action) VALUES ('2', '2021-01-22 00:00:00', 'confirmed');
 INSERT INTO Confirmations (user_id, time_stamp, action) VALUES ('2', '2021-02-28 23:59:59', 'timeout');
+
+--------------
+-- SOLUCIÓN --
+--------------
+
+SELECT 
+  S.user_id, 
+  ROUND(COALESCE(C.solicitudes_confirmadas, 0) / COALESCE(C.solicitudes_totales, 1), 2) AS confirmation_rate
+FROM Signups S
+LEFT JOIN (
+  SELECT
+    user_id,
+    COUNT(*) AS solicitudes_totales,
+    SUM(action = 'confirmed') AS solicitudes_confirmadas
+FROM
+    Confirmations
+GROUP BY
+    user_id
+) C ON C.user_id = S.user_id;
+
+-- Explicación: --
+-- Este query lista las solicitudes totales y las confirmadas para cada usuario:
+SELECT
+    user_id,
+    COUNT(*) AS solicitudes_totales,
+    SUM(action = 'confirmed') AS solicitudes_confirmadas
+FROM
+    Confirmations
+GROUP BY
+    user_id;
+-- Hacemos un LEFT JOIN con Signups para tener la lista de todos los usuarios:
+SELECT 
+  S.user_id, 
+  C.solicitudes_confirmadas / C.solicitudes_totales AS confirmation_rate
+FROM Signups S
+LEFT JOIN (
+  SELECT
+    user_id,
+    COUNT(*) AS solicitudes_totales,
+    SUM(action = 'confirmed') AS solicitudes_confirmadas
+FROM
+    Confirmations
+GROUP BY
+    user_id
+) C ON C.user_id = S.user_id; -- el user con id 6 sale NULL
+-- Usamos la función COALESCE para colocar valores cuando tengamos NULL para poder dividir:
+SELECT 
+  S.user_id, 
+  COALESCE(C.solicitudes_confirmadas, 0) / COALESCE(C.solicitudes_totales, 1) AS confirmation_rate
+FROM Signups S
+LEFT JOIN (
+  SELECT
+    user_id,
+    COUNT(*) AS solicitudes_totales,
+    SUM(action = 'confirmed') AS solicitudes_confirmadas
+FROM
+    Confirmations
+GROUP BY
+    user_id
+) C ON C.user_id = S.user_id; -- ahora el NULL del user 6 pasa a 0.0
+-- Aplicamos ROUND(..., 2) para redondear a 2 decimales:
+SELECT 
+  S.user_id, 
+  ROUND(COALESCE(C.solicitudes_confirmadas, 0) / COALESCE(C.solicitudes_totales, 1), 2) AS confirmation_rate
+FROM Signups S
+LEFT JOIN (
+  SELECT
+    user_id,
+    COUNT(*) AS solicitudes_totales,
+    SUM(action = 'confirmed') AS solicitudes_confirmadas
+FROM
+    Confirmations
+GROUP BY
+    user_id
+) C ON C.user_id = S.user_id;
+-----------------------------
+-- Alternativa de solución:
+SELECT
+    s.user_id,
+    ROUND(COALESCE(c.confirmed_count, 0) / COALESCE(c.total_count, 1), 2) AS confirmation_rate
+FROM
+    Signups s
+LEFT JOIN
+    (
+        SELECT
+            user_id,
+            COUNT(*) AS total_count,
+            SUM(action = 'confirmed') AS confirmed_count
+        FROM
+            Confirmations
+        GROUP BY
+            user_id
+    ) c ON s.user_id = c.user_id;
+-- Referencia: https://leetcode.com/problems/confirmation-rate/solutions/3608649/mysql-solution-for-confirmation-rate-problem/?envType=study-plan-v2&envId=top-sql-50
+
