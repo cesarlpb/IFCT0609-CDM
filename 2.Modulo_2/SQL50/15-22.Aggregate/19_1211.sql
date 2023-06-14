368a75d1c04fe14aa5c88e327601225e3e90ce4a
César <cesarlpb@gmail.com>
2023-06-14 19:27:21 +0200

    Ej 19 - CREATE TABLE Queries

--- 2.Modulo_2/SQL50/15-22.Aggregate/19_1211.sql
+++ 2.Modulo_2/SQL50/15-22.Aggregate/19_1211.sql
@@ -18,3 +18,13 @@ Devuelve la tabla de resultados en cualquier orden.
 
 El formato del resultado de la consulta está en el siguiente ejemplo.
 */
+
+CREATE TABLE IF NOT EXISTS Queries (query_name VARCHAR(30), result VARCHAR(50), position INT, rating INT);
+TRUNCATE TABLE Queries;
+INSERT INTO Queries (query_name, result, position, rating) VALUES ('Dog', 'Golden Retriever', '1', '5');
+INSERT INTO Queries (query_name, result, position, rating) VALUES ('Dog', 'German Shepherd', '2', '5');
+INSERT INTO Queries (query_name, result, position, rating) VALUES ('Dog', 'Mule', '200', '1');
+INSERT INTO Queries (query_name, result, position, rating) VALUES ('Cat', 'Shirazi', '5', '2');
+INSERT INTO Queries (query_name, result, position, rating) VALUES ('Cat', 'Siamese', '3', '3');
+INSERT INTO Queries (query_name, result, position, rating) VALUES ('Cat', 'Sphynx', '7', '4');
+
