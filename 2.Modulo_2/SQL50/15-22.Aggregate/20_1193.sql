3a469f0a1829b1fddb6ce15fced449722650db53
César <cesarlpb@gmail.com>
2023-06-14 19:31:54 +0200

    Ej 20 CREATE TABLE Transactions

--- 2.Modulo_2/SQL50/15-22.Aggregate/20_1193.sql
+++ 2.Modulo_2/SQL50/15-22.Aggregate/20_1193.sql
@@ -7,3 +7,10 @@ Devuelve la tabla de resultados en cualquier orden.
 
 El formato del resultado de la consulta está en el siguiente ejemplo.
 */
+
+CREATE TABLE IF NOT EXISTS Transactions2 (id INT, country VARCHAR(4), state enum('approved', 'declined'), amount INT, trans_DATE DATE);
+TRUNCATE TABLE Transactions2;
+INSERT INTO Transactions2 (id, country, state, amount, trans_DATE) VALUES ('121', 'US', 'approved', '1000', '2018-12-18');
+INSERT INTO Transactions2 (id, country, state, amount, trans_DATE) VALUES ('122', 'US', 'declined', '2000', '2018-12-19');
+INSERT INTO Transactions2 (id, country, state, amount, trans_DATE) VALUES ('123', 'US', 'approved', '2000', '2019-01-01');
+INSERT INTO Transactions2 (id, country, state, amount, trans_DATE) VALUES ('124', 'DE', 'approved', '2000', '2019-01-07');
