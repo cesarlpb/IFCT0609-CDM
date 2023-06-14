98b4cf0426f3f6e63da5b83a89211e2bca379e77
César <cesarlpb@gmail.com>
2023-06-14 19:00:09 +0200

    CREATE TABLE Project y Employee

--- 2.Modulo_2/SQL50/15-22.Aggregate/17_1075.sql
+++ 2.Modulo_2/SQL50/15-22.Aggregate/17_1075.sql
@@ -10,3 +10,17 @@ Devuelva la tabla de resultados en cualquier orden.
 El formato del resultado de la consulta está en el siguiente ejemplo.
 
 */
+
+CREATE TABLE IF NOT EXISTS Project (project_id INT, employee_id INT);
+CREATE TABLE IF NOT EXISTS Employee3 (employee_id INT, name VARCHAR(10), experience_years INT);
+TRUNCATE TABLE Project;
+INSERT INTO Project (project_id, employee_id) VALUES ('1', '1');
+INSERT INTO Project (project_id, employee_id) VALUES ('1', '2');
+INSERT INTO Project (project_id, employee_id) VALUES ('1', '3');
+INSERT INTO Project (project_id, employee_id) VALUES ('2', '1');
+INSERT INTO Project (project_id, employee_id) VALUES ('2', '4');
+TRUNCATE TABLE Employee3;
+INSERT INTO Employee3 (employee_id, name, experience_years) VALUES ('1', 'Khaled', '3');
+INSERT INTO Employee3 (employee_id, name, experience_years) VALUES ('2', 'Ali', '2');
+INSERT INTO Employee3 (employee_id, name, experience_years) VALUES ('3', 'John', '1');
+INSERT INTO Employee3 (employee_id, name, experience_years) VALUES ('4', 'Doe', '2');
