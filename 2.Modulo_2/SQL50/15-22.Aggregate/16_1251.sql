92e86fa74eaec61de6df25c2becb648bb1d01fd4
César <cesarlpb@gmail.com>
2023-06-14 18:57:43 +0200

    Ej 16 - CREATE TABLE Prices y UnitsSold

--- 2.Modulo_2/SQL50/15-22.Aggregate/16_1251.sql
+++ 2.Modulo_2/SQL50/15-22.Aggregate/16_1251.sql
@@ -8,3 +8,16 @@ Devuelva la tabla de resultados en cualquier orden.
 
 El formato del resultado de la consulta está en el siguiente ejemplo.
 */
+
+CREATE TABLE IF NOT EXISTS Prices (product_id INT, start_DATE DATE, end_DATE DATE, price INT);
+CREATE TABLE IF NOT EXISTS UnitsSold (product_id INT, purchase_DATE DATE, units INT);
+TRUNCATE TABLE Prices;
+INSERT INTO Prices (product_id, start_DATE, end_DATE, price) VALUES ('1', '2019-02-17', '2019-02-28', '5');
+INSERT INTO Prices (product_id, start_DATE, end_DATE, price) VALUES ('1', '2019-03-01', '2019-03-22', '20');
+INSERT INTO Prices (product_id, start_DATE, end_DATE, price) VALUES ('2', '2019-02-01', '2019-02-20', '15');
+INSERT INTO Prices (product_id, start_DATE, end_DATE, price) VALUES ('2', '2019-02-21', '2019-03-31', '30');
+TRUNCATE TABLE UnitsSold;
+INSERT INTO UnitsSold (product_id, purchase_DATE, units) VALUES ('1', '2019-02-25', '100');
+INSERT INTO UnitsSold (product_id, purchase_DATE, units) VALUES ('1', '2019-03-01', '15');
+INSERT INTO UnitsSold (product_id, purchase_DATE, units) VALUES ('2', '2019-02-10', '200');
+INSERT INTO UnitsSold (product_id, purchase_DATE, units) VALUES ('2', '2019-03-22', '30');
