3a0bb87f914fb988d6a7c58bf4a39d0dca796072
César <cesarlpb@gmail.com>
2023-06-14 18:46:45 +0200

    Ej 15 - función MOD para calcular módulo (resto)

--- 2.Modulo_2/SQL50/15-22.Aggregate/15_620.sql
+++ 2.Modulo_2/SQL50/15-22.Aggregate/15_620.sql
@@ -8,3 +8,27 @@ Devuelva la tabla de resultados ordenada por calificación en orden descendente.
 
 El formato del resultado de la consulta está en el siguiente ejemplo.
 */
+
+CREATE TABLE IF NOT EXISTS cinema (id INT, movie VARCHAR(255), description VARCHAR(255), rating FLOAT(2, 1));
+TRUNCATE TABLE cinema;
+INSERT INTO cinema (id, movie, description, rating) VALUES ('1', 'War', 'great 3D', '8.9');
+INSERT INTO cinema (id, movie, description, rating) VALUES ('2', 'Science', 'fiction', '8.5');
+INSERT INTO cinema (id, movie, description, rating) VALUES ('3', 'irish', 'boring', '6.2');
+INSERT INTO cinema (id, movie, description, rating) VALUES ('4', 'Ice song', 'Fantacy', '8.6');
+INSERT INTO cinema (id, movie, description, rating) VALUES ('5', 'House card', 'Interesting', '9.1');
+
+--------------
+-- SOLUCIÓN --
+--------------
+SELECT * FROM Cinema
+WHERE 
+  MOD(id, 2) = 1 AND
+  description != 'boring'
+  ORDER BY rating DESC;
+
+-- Explicación
+-- Mostrando los ids impares:
+SELECT id, MOD(id, 2) FROM Cinema
+WHERE MOD(id, 2) = 1;
+-- Descartamos descripcion 'boring
+-- Ordenamos por rating descendente
