72285476db7b7834210ad8410d4fc2c307404530
César <cesarlpb@gmail.com>
2023-06-15 20:24:52 +0200

    Script para guardar los datos en la tabla Subastas

--- 2.Modulo_2/proyectos/1.subastas/scrapper/save.js
+++ 2.Modulo_2/proyectos/1.subastas/scrapper/save.js
@@ -1,13 +1,51 @@
 const mysql = require("mysql");
+const fs = require("fs");
 
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "", 
-  database: ""
+  database: "proyectos"
 });
+
+createTableQuery = `
+CREATE TABLE IF NOT EXISTS Subastas (
+  subastasId SMALLINT NOT NULL AUTO_INCREMENT , 
+  ubicacion VARCHAR(255) NOT NULL , 
+  expedienteId VARCHAR(255) NOT NULL , 
+  estado VARCHAR(255) NOT NULL , 
+  descripcion VARCHAR(65535) NULL , 
+  PRIMARY KEY (subastasId)) ENGINE = InnoDB;
+`;
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  // Crear tabla si no existe
+  con.query(createTableQuery, function (err, result) {
+    if (err) throw err;
+    console.log("Tabla Subastas creada")
+  });
+  fs.readFile('data.json', 'utf8', function(err, data){
+    if (err) throw err;
+    console.log(data)
+    let arr = JSON.parse(data) // Transformamos texto en objeto JSON
+    for(let indice in arr){
+      let obj = arr[indice]
+      let insertQuery = `
+      INSERT INTO Subastas 
+      (subastasId, ubicacion, expedienteId, estado, descripcion)
+      VALUES
+      (
+        NULL,
+        ?, ?, ?, ?
+      )`.replaceAll("\n", "");
+      con.query(
+        insertQuery, 
+        [obj.ubicacion,obj.expedienteId, obj.estado,obj.descripcion],
+        function (err, result) {
+        if (err) throw err;
+        console.log(`fila con id ${result.insertId} insertada`);
+      });
+    }
+  })
 });
