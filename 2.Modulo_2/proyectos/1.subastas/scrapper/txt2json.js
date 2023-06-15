52679850c10958717f46a038095d1224f1efd1d2
César <cesarlpb@gmail.com>
2023-06-15 18:38:23 +0200

    Convertimos un txt en JSON

--- 2.Modulo_2/proyectos/1.subastas/scrapper/txt2json.js
+++ 2.Modulo_2/proyectos/1.subastas/scrapper/txt2json.js
@@ -12,5 +12,28 @@ fs.readFile('2.txt', 'utf8', function(err, data){
             objArr.push(item.trim())
         }
     })
-    console.log(objArr);
+    // Creamos el JSON
+    let json = {}
+    // subastaId
+    // 'SUBASTA SUB-JA-2023-209686 (25 lotes)' -> SUB-JA-2023-209686
+    let subastaId = objArr[0].split(" ")[1]
+    // 'JUZGADO MERCANTIL 3 - MADRID'
+    let ubicacion = objArr[1]
+    // 'Expediente: 0549/14',
+    let expedienteId = objArr[2].split(" ")[1]
+    // Estado: Celebrándose - [Conclusión prevista: 03/07/2023 a las 18:00:00]
+    let estado = objArr[3].split(' ').slice(1).join(' ');
+    // descripción: bucle que agregue todos los strings desde 4 en adelante
+    let descripcion = "";
+    for(let i = 4; i<objArr.length; i++){
+        descripcion += objArr[i]
+    }
+    // Añadimos todos los campos al json:
+    json["subastaId"] = subastaId;
+    json["ubicacion"] = ubicacion;
+    json["expedienteId"] = expedienteId;
+    json["estado"] = estado;
+    json["descripcion"] = descripcion;
+
+    console.log(JSON.stringify(json, null, 2)) // ok
 });
