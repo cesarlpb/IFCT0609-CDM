256e56294a2c0675421eb773b2ff958a29a6da10
César <cesarlpb@gmail.com>
2023-06-19 20:33:27 +0200

    Node - Ejercicio 2

--- 2.Modulo_2/examenes/server.js
+++ 2.Modulo_2/examenes/server.js
@@ -9,16 +9,28 @@ const con = mysql.createConnection({
 });
 
 // Queries
-const selectAll = "SELECT * FROM tblUsuarios;";
+const selectAll = "SELECT * FROM tblUsuarios LIMIT 10;";
+const selectOne = "SELECT * FROM tblUsuarios WHERE idx = ?;";
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Conexión exitosa a la base de datos " + "examen");
   http.createServer(function(req, res){
-    if(req.url.startsWith("/usuarios")){
+    let id = req.url.split("/")[2]
+    if(req.url.startsWith("/usuarios") && !id){
       con.query(selectAll, function (err, result) {
         if (err) throw err;
         console.log("Lista de datos")
+        res.writeHead(200, {"Content-type" : "application/json; charset = UTF-8;"})
+        res.end(JSON.stringify(result, null, 2))
+      })
+    }else if(req.url.startsWith("/usuarios/") && id){
+      con.query(
+        selectOne, 
+        [id],
+        function (err, result) {
+        if (err) throw err;
+        console.log("Un dato")
         res.writeHead(200, {"Content-type" : "application/json; charset = UTF-8;"})
         res.end(JSON.stringify(result, null, 2))
       })
