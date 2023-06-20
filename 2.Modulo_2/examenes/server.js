1535b14796778e7aa21e4669f8c245492892af36
César <cesarlpb@gmail.com>
2023-06-20 19:57:22 +0200

    update

--- 2.Modulo_2/examenes/server.js
+++ 2.Modulo_2/examenes/server.js
@@ -5,7 +5,7 @@ const con = mysql.createConnection({
   host : "localhost",
   user : "root",
   password : "",
-  database : "pruebas" // <-- colocad la db correcta
+  database : "db" // <-- colocad la db correcta
 });
 
 // Queries
@@ -15,26 +15,61 @@ const postQuery = "INSERT INTO tblUsuarios (idx, usuario, nombre, sexo, nivel, e
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Conexión exitosa a la base de datos " + con.config.database);
+  console.log("Conexión exitosa a la base de datos " + "examen");
   http.createServer(function(req, res){
     let id = req.url.split("/")[2]
     if(req.url.startsWith("/usuarios") && !id && req.method == "GET"){
-      res.end("GET")
-      /* Escribe aquí la lógica del endpoint */ 
+      con.query(selectAll, function (err, result) {
+        if (err) throw err;
+        console.log("Lista de datos")
+        res.writeHead(200, {"Content-type" : "application/json; charset = UTF-8;"})
+        res.end(JSON.stringify(result, null, 2))
+      })
     }else if(req.url.startsWith("/usuarios/") && id && req.method == "GET"){
-      res.end("GET por id")
-      /* Escribe aquí la lógica del endpoint */ 
+      con.query(
+        selectOne, 
+        [id],
+        function (err, result) {
+        if (err) throw err;
+        console.log("Un dato")
+        res.writeHead(200, {"Content-type" : "application/json; charset = UTF-8;"})
+        res.end(JSON.stringify(result, null, 2))
+      })
     }else if(req.url.startsWith("/usuarios") && !id && req.method == "POST"){
-      res.end("POST")
-      /* Escribe aquí la lógica del endpoint */ 
+      let jsonString = ""
+      req.on('data', function (data) {
+        jsonString += data;
+      });
+      req.on('end', function () {
+        json = JSON.parse(jsonString);
+        let idx = json.idx;
+        let usuario = json.usuario;
+        let nombre = json.nombre;
+        let sexo = json.sexo;
+        let nivel = json.nivel;
+        let email = json.email;
+        let telefono = json.telefono;
+        let marca = json.marca;
+        let compañia = json.compañia;
+        let saldo = json.saldo;
+        let activo = json.activo;
+        con.query(
+          postQuery, 
+          [idx, usuario, nombre, sexo, nivel, email, telefono, marca, compañia, saldo, activo], 
+          function (err, result) {
+            if (err) throw err;
+            console.log("Se ha creado " + result.affectedRows + " dato con id " + result.insertId)
+            res.writeHead(201, {"Content-type" : "application/json; charset = UTF-8;"})
+            res.end()
+          })
+        })
+    }else if(req.method == "PUT"){
+      // --- No entra en el examen del 21-06: ---
+      res.end("PUT. Coming soon in best repos.🐱‍👤")
+    }else if(req.method == "DELETE"){
+      // --- No entra en el examen del 21-06: ---
+      res.end("DELETE. Coming soon in best repos.🐱‍👤")
     }
-    // --- No entra en examen del 21-06: --- //
-    // else if(req.method == "PUT"){
-    //   res.end("PUT. Coming soon in best repos.🐱‍👤")
-    // }else if(req.method == "DELETE"){
-    //   res.end("DELETE. Coming soon in best repos.🐱‍👤")
-    // }
-    // --- No entra en examen del 21-06 ^^^ --- // 
     else{
       res.writeHead(400)
       res.end("Bad Request")
@@ -43,7 +78,7 @@ con.connect(function(err) {
     console.log("GET de lista: http://localhost:8080/usuarios")
     console.log("GET por id: http://localhost:8080/usuarios/1")
     console.log("POST por id: http://localhost:8080/usuarios")
-    // No entra en examen del 21-06:
+    // --- No entra en el examen del 21-06: ---
     // console.log("PUT por id: http://localhost:8080/usuarios/1")
     // console.log("DELETE por id: http://localhost:8080/usuarios/1")
   });
