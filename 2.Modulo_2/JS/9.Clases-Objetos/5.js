f454ce55a01f779e72f1c92c8401c0f415abd630
César <cesarlpb@gmail.com>
2023-06-22 20:52:32 +0200

    Ej 5, clase Ficha - código inicial

---
+++ 2.Modulo_2/JS/9.Clases-Objetos/5.js
@@ -0,0 +1,17 @@
+/*
+Crea una clase Ficha, con su constructor, que se usará para almacenar el número de kilómetros recorridos por una persona en cada sesión de ejercicios. Las propiedades de la clase serán
+nombre
+sesiones
+numsesioens
+El nombre es el de la persona, en los sesiones se almacenará el numero de kilometros recorridos en cada sesión y numsesiones contien el númeor de sesiones anotadas.
+
+Tiene dos métodos:
+
+anotar: anota los kilómetros
+media: calcula la media de kilómetros recorridos
+*/
+class Ficha{
+  // constructor
+  // método anotar
+  // método media
+}
