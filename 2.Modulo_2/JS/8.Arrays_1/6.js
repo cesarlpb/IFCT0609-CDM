f351779892f00e3bbe3b036d39beaaccb2a7fabe
César <cesarlpb@gmail.com>
2023-06-22 16:14:15 +0200

    Ej 6, comprobar si letra está en array

--- 2.Modulo_2/JS/8.Arrays_1/6.js
+++ 2.Modulo_2/JS/8.Arrays_1/6.js
@@ -4,3 +4,17 @@ Escribe una función que determina si la letra que se le pasa como argumento se 
 Se trata de resolverlo sin usar métodos del objeto array, solo el dato tipo array.
 */
 
+function verificarLetra(letra, arr){
+  for(let i = 0; i<arr.length; i++){
+    if(arr[i] == letra){
+      return true
+    }
+  }
+  return false
+}
+let letra = "a"
+let arr = ["a", "b", "c"]
+console.log(letra, arr, "letra en arr?" + verificarLetra(letra, arr)) // true
+letra = "z"
+console.log(letra, arr, "letra en arr? " + verificarLetra(letra, arr)) // false
+
