1cf3230d52ea4abcbec3819fe64bbc269f707b6f
César <cesarlpb@gmail.com>
2023-06-22 16:51:28 +0200

    Ej 10, Fn para buscar precios de tienda de productos usando un array 2D

--- 2.Modulo_2/JS/8.Arrays_1/10.js
+++ 2.Modulo_2/JS/8.Arrays_1/10.js
@@ -4,4 +4,30 @@ Un tienda vende monitores, teclados y ratones. Los precios se almacenan en una e
 Diseña una función que reciba como argumento el nombre de un producto (minúscula) y devuelva su precio.
 */
 
+const datos = [
+  ["monitor LG", 125],
+  ["teclado gaming", 50],
+  ["ratón Corsair", 19.99]
+]
+
+function buscarPrecio(producto){
+  for(let i = 0; i<datos.length; i++){
+    let dato = datos[i] // arr -> 0 es el nombre del producto, 1 -> precio
+    let nombreProducto = dato[0]
+    if(nombreProducto.includes(producto)){
+      return dato[1] // precio
+    }else{
+      return NaN
+    }
+  }
+}
+let producto = "monitor LG"
+let precio = buscarPrecio(producto)
+console.log(producto, precio) // 125
+producto = "monitor"
+precio = buscarPrecio(producto)
+console.log(producto, precio) // 125
+producto = "monitor$"
+precio = buscarPrecio(producto)
+console.log(producto, precio) // NaN
 
