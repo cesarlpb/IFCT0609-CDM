789360cd7aa59bbfbd82ced6d89e0f2f5a1f6837
César <cesarlpb@gmail.com>
2023-06-22 16:58:23 +0200

    Ej 8, Separar por guiones especificando caracter

--- 2.Modulo_2/JS/8.Arrays_1/8.js
+++ 2.Modulo_2/JS/8.Arrays_1/8.js
@@ -1,3 +1,21 @@
 /*
-Escribe una función qeu reciba como argumento un array y que devuelva una cadena de caracteres formada por los elementos del array separados por un guión
+Escribe una función que reciba como argumento un array y que devuelva una cadena de caracteres formada por los elementos del array separados por un guión -> añadimos que se pueda elegir el caracter para concatenar
 */
+
+function separarPorGuiones2(arr, char = "-"){
+  let str = ""
+  for(let i = 0; i<arr.length; i++){
+    if(i!=arr.length-1){
+      str += arr[i] + char
+    }else{
+      str += arr[i]
+    }
+  }
+  return str // Quitamos el último guión que me coloca el bucle
+}
+let arr = [1, 2, 3]
+let str = separarPorGuiones2(arr) // -
+console.log(str)
+arr = [1, 2, 3]
+str = separarPorGuiones2(arr, "-o-") // -
+console.log(str)
