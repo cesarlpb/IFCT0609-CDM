aa57a2d29bafb931b765d8186b870ae52225965d
César <cesarlpb@gmail.com>
2023-06-22 16:03:55 +0200

    JS - Arrays, enunciados del 7 al 10

---
+++ 2.Modulo_2/JS/8.Arrays_1/7.js
@@ -0,0 +1,6 @@
+/*
+En este ejempo debes crear una función que reciba una array y devuelva una cadena formada por los elementos del array separados por un guión - si una letra es una vocal. La función tiene como único parámetro la letra que se quiere comprobar. Debe funcionar igual con mayúsculas y con minúsculas y por supuesto usando arrays.
+
+Solo se aceptan letras internacionales (sin acentos)
+*/
+
---
+++ 2.Modulo_2/JS/8.Arrays_1/8.js
@@ -0,0 +1,3 @@
+/*
+Escribe una función qeu reciba como argumento un array y que devuelva una cadena de caracteres formada por los elementos del array separados por un guión
+*/
