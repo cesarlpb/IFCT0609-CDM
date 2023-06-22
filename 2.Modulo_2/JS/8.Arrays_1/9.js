cfbaee821b1006cdc31ad5bfed4c48d7b000e237
César <cesarlpb@gmail.com>
2023-06-22 18:07:40 +0200

    Ej 9, buscar encargado en un array 2D

--- 2.Modulo_2/JS/8.Arrays_1/9.js
+++ 2.Modulo_2/JS/8.Arrays_1/9.js
@@ -6,3 +6,24 @@ Usa un array para almacenar los datos del servicio. En este array cada elemento 
 Crea una función que responda con el nombre de la persona encargada del servicio sabiendo el día. Si el día no existe deberá decir que no hay servicio. Los nombres de los días deben ir en minúsculas.
 */
 
+const horarios = [
+  ["lunes", "María"], 
+  ["martes", "Luis"], 
+  ["miércoles", "Antonia"], 
+  ["jueves", "Pedro"], 
+  ["viernes", "Marisa"]
+]
+
+function buscarEncargado(diaSemana){
+  for(let i = 0; i<horarios.length; i++){
+    let [dia, encargado] = horarios[i]
+    if(dia == diaSemana.toLowerCase()){
+      return encargado
+    }
+  }
+  return "No hay servicio"
+}
+let dia = "martes"
+console.log(buscarEncargado(dia)) // Luis
+dia = "sábado"
+console.log(buscarEncargado(dia)) // No hay servicio
