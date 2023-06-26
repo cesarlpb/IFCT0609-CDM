a97f5727ed97021aa8188ad02067ec97105c84e0
César <cesarlpb@gmail.com>
2023-06-26 18:28:22 +0200

    Ej 8 - Clase Alumno

--- 2.Modulo_2/JS/9.Clases-Objetos/8.js
+++ 2.Modulo_2/JS/9.Clases-Objetos/8.js
@@ -5,3 +5,25 @@ El objeto tiene un método para poner la calificación: calificar(nota).
 
 Crea el script para gestionar un grupo de 4 alumnos.
 */
+
+class Alumno{
+  constructor(
+    nombre_ = "",
+    matricula_ = "",
+    nota_ = 0
+  ){
+    this.nombre = nombre_
+    this.matricula = matricula_
+    this.nota = nota_
+  }
+  calificar(nota){
+    if(nota >= 0 && nota <= 10){
+      this.nota = nota
+    }
+  }
+}
+let alumno1 = new Alumno("Pepe", "234")
+alumno1.calificar(5)
+let alumno2 = new Alumno("Pepa", "789", 8.5)
+let alumno3 = new Alumno("Pepo", "785", 7)
+let alumno4 = new Alumno("Pepu", "123", 7.75)
