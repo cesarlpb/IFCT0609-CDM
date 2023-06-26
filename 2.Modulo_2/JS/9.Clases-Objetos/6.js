e7ff79cc920316f9ba9ce78ea777e3dddbacd79a
César <cesarlpb@gmail.com>
2023-06-26 16:18:02 +0200

    Ej 6 - código inicial

---
+++ 2.Modulo_2/JS/9.Clases-Objetos/6.js
@@ -0,0 +1,23 @@
+/*
+Crea una clase que llamaremos Bus. Sus atributos serán:
+
+capacidad: número máximo de pasajeros
+pasajeros: número de pasajeros (inicialmente 0)
+conductor: objeto conductor.
+
+Sus métodos:
+
+subir(pasajeros): aumenta el numero de pasajeros
+bajar(pasajaeros): disminuye el número de pasajeros
+conductor: asigna un objeto conductor.
+El objeto conductor es de una clase (Conductor) cuyos atributos son:
+
+nombre: nombre del conductor
+licencia: un número que identifica al condcutor.
+
+Restricciones:
+
+Al crear el objeto Bus se asigna también el Conductor.+
+
+No pueden subir más pasajeros que los máximos admitidos y no pueden bajar más de los que hay.
+*/
