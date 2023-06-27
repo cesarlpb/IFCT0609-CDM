50b0574b78f8f10c0dde95a5c926205ddf737592
César <cesarlpb@gmail.com>
2023-06-27 15:52:46 +0200

    Ej 6 - código inicial

---
+++ 2.Modulo_2/JS/10.DOM/6.js
@@ -0,0 +1,8 @@
+/*
+En este script debes leer todos los elementos tipo imagen de la página y mostrar sus urls en un bloque tipo div cuyo id es fuentes y que ya existe en la página modelo usada para estos ejercicios. El script se ejecutará al pulsar el botón con el texto fuentes
+*/
+
+// querySelectorAll -> img -> arr
+// bucle -> img -> src -> enlace
+// html -> <ul> bucle-> <li> src </li> </ul>
+// añadimos el ul en un #fuentes
