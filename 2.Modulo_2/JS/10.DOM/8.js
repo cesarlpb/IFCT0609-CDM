4fb05f6f9cf51f3d14e33fc275a07a5a41caaf55
César <cesarlpb@gmail.com>
2023-06-27 17:59:15 +0200

    Ej 8 - Insertar div entre <header> y titular

--- 2.Modulo_2/JS/10.DOM/8.js
+++ 2.Modulo_2/JS/10.DOM/8.js
@@ -1,3 +1,13 @@
 /*
-Escribe el código de una función nuevoBloque() que deberá crear un nuevo elemento div, con una class de nombre destacar. El bloque contendrá, en negrita, el texto Página Modelo. Este bloque debe aparecer justo antes del bloque subtitulo es decir justo después del titular de la página. Se activará con el botón superior que contiene el texto Subtitulo
+Escribe el código de una función intercalar() que deberá crear un nuevo elemento div, con una class de nombre destacar. El bloque contendrá, en negrita, el texto Página Modelo. Este bloque debe aparecer justo antes del bloque subtitulo es decir justo después del titular de la página. Se activará con el botón superior que contiene el texto Subtitulo
 */
+function intercalar(){
+  // Alternativa: Crear todo el elemento con string multilínea, usar DOMParser() para pasarlo a objeto NodeElement (más info: https://davidwalsh.name/convert-html-stings-dom-nodes) y usar insertAdjacentHTML(...)
+  let header = document.querySelector("header");
+  let div = document.createElement("div", );
+  div.setAttribute("id", "div-intercalado");
+  let strong = document.createElement("strong");
+  strong.innerText = "Página Modelo";
+  div.appendChild(strong);
+  header.insertAdjacentElement("afterend", div);
+} 
