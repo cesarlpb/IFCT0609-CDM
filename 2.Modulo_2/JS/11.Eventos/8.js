5a9e28c2a98f019d0acd9df663d470eb7a480339
César <cesarlpb@gmail.com>
2023-06-28 18:29:45 +0200

    Ej 8 - código inicial

---
+++ 2.Modulo_2/JS/11.Eventos/8.js
@@ -0,0 +1,3 @@
+/*
+En esta página existen tres bloques ocultos. Debes crear un evento de teclado para que actúe cuando el usuario pulsa una tecla. Si pulsa la tecla 1 se abre el bloque con id ficha1, si pulsa 2 se abre el bloque ficha2 y si 3 se abre el de ficha3.
+*/
