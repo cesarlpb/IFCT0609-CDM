7a38ab8ac1705107bccd759f620ffd5cf78a8641
César <cesarlpb@gmail.com>
2023-06-28 16:12:34 +0200

    Ej 7 - codigo inicial

---
+++ 2.Modulo_2/JS/11.Eventos/7.js
@@ -0,0 +1,6 @@
+/*
+En la página que puedes construir con este código HTML tienes tres bloques: uno de color rojo otro de color blanco y otro de color azul. Crea un script para que cuando el ratónse mueve desde el bloque rojo al central éste se pongo rojo. Si se mueve desde el azul al central, éste se pondrá azul. Si entra en el bloque desde otro sitio el bloque recuperará el color blanco.
+*/
+function ej7(){
+  
+}
