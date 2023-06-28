c130c05f6c4afb99578793abb334656fbdabe3dc
César <cesarlpb@gmail.com>
2023-06-28 15:50:28 +0200

    Ej 6 - código inicial

---
+++ 2.Modulo_2/JS/11.Eventos/6.js
@@ -0,0 +1,7 @@
+/*
+La página descrita con este código HTML posee dos bloques div cuyos atributos id son cX, cY. Debes escribir un manejador para el evento de movimiento del ratón por el documento. Este script deberá anotar en los cuadros cX y cY las coordenadas donde se encuentra el puntero del ratón mientras se mueve por todo el documento.
+*/
+
+function ej6(){
+  
+}
