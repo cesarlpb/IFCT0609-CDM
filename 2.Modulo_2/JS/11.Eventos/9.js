def4e926cb5fc68cc6f637c5467ac9b7988046d2
César <cesarlpb@gmail.com>
2023-06-28 19:00:16 +0200

    Ej 9 - evento scroll para botón que devuelve a top

--- 2.Modulo_2/JS/11.Eventos/9.js
+++ 2.Modulo_2/JS/11.Eventos/9.js
@@ -1,3 +1,18 @@
 /*
 Escribe un script que responda al evento scroll y que muestre un boton o bloque div en el fondo de la pantalla cuando deje de verse la linea superior del documento
 */
+function ej9(){
+  let boton = document.getElementById("boton")
+  document.addEventListener("scroll", function(){
+    // console.log("innerHeight:", window.innerHeight) // px
+    console.log("scrollY:", window.scrollY) // px
+    if(window.scrollY > 105){
+      boton.classList.remove("hidden")
+      boton.classList.add("boton-fijo")
+    }else{
+      boton.classList.add("hidden")
+      boton.classList.remove("boton-fijo")
+    }
+  })
+}
+ej9()
