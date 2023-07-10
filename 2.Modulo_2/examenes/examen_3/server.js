f97ed2ac2e77a5f79005af1d886d57d65f239b57
César <cesarlpb@gmail.com>
2023-07-10 17:00:38 +0200

    Enpoints GET y POST

--- 2.Modulo_2/examenes/examen_3/server.js
+++ 2.Modulo_2/examenes/examen_3/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 const posts = [
   {id: 1, title: "Título", content: "Contenido"}
@@ -7,6 +12,11 @@ const posts = [
 
 app.get("/", (req, res) => {
   res.json(posts)
+})
+app.post("/", (req, res) => {
+  let nuevoPost = req.body // objeto
+  posts.push(nuevoPost)
+  res.status(201).json(nuevoPost)
 })
 
 app.listen(3000, () => {
